6f2a65c3a336125d1a058d96967b523972fe6230
emperorbj <cwaku96@gmail.com>
2024-04-28 13:32:49 -0700

    added comment on metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-
+// for SEO performance
 export const metadata: Metadata = {
   title: "blogger",
   description: "best blog app for creators",
